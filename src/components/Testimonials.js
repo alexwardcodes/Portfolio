c63ad7f0577480a6e2104615d27cc8977356ed52
alexwardcodes <amwtranslations@gmail.com>
2022-11-24 12:39:05 +0000

    Changed to simply Testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,7 +7,7 @@ export default function Testimonials (props) {
         <div className="text-container">
           <div className="row">
             <div className="two columns header-col">
-              <h1><span>Client Testimonials</span></h1>
+              <h1><span>Testimonials</span></h1>
             </div>
             <div className="ten columns flex-container">
               <div className="flexslider">
