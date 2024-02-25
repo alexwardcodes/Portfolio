c5eb2a107de85290c97e208b052efc5164c73dec
alexwardcodes <amwtranslations@gmail.com>
2024-02-25 23:33:56 +0000

    Adjusted mobile styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,88 +14,88 @@ const Resume = () => {
 
 		  <div className="inline">
             <Icon icon="teenyicons:typescript-outline" id="icon" width="75" />
-            <div>TypeScript</div>
+            <div className="skill-name">TypeScript</div>
           </div>
           <div className="inline">
             <Icon icon="mdi:language-javascript" id="icon" width="75" />
-            <div>JavaScript</div>
+            <div className="skill-name">JavaScript</div>
           </div>
 		  <div className="inline">
             <Icon icon="teenyicons:react-outline" id="icon" width="75" />
-            <div>React.js</div>
+            <div className="skill-name">React.js</div>
           </div>
           <div className="inline">
             <Icon icon="la:node-js" id="icon" width="75" />
-            <div>Node.js</div>
+            <div className="skill-name">Node.js</div>
           </div>
           <div className="inline">
             <Icon icon="simple-icons:express" id="icon" width="75" />
-            <div>Express.js</div>
+            <div className="skill-name">Express.js</div>
           </div>
 		  <div className="inline">
             <Icon icon="simple-icons:jest" id="icon" width="75" />
-            <div>Jest</div>
+            <div className="skill-name">Jest</div>
           </div>
           <div className="inline">
             <Icon icon="simple-icons:cypress" id="icon" width="75" />
-            <div>Cypress</div>
+            <div className="skill-name">Cypress</div>
           </div>
           <div className="inline">
             <Icon icon="simple-icons:playwright" id="icon" width="75" />
-            <div>Playwright</div>
+            <div className="skill-name">Playwright</div>
           </div>
           <div className="inline">
             <Icon icon="simple-icons:sonarqube" id="icon" width="75" />
-            <div>Sonar Qube</div>
+            <div className="skill-name">Sonar Qube</div>
           </div>
           <div className="inline">
             <Icon icon="mdi:jquery" id="icon" width="75" />
-            <div>jQuery</div>
+            <div className="skill-name">jQuery</div>
           </div>
 		  <div className="inline">
             <Icon icon="ion:logo-html5" id="icon" width="75" />
-            <div>HTML</div>
+            <div className="skill-name">HTML</div>
           </div>
           <div className="inline">
             <Icon icon="ion:logo-css3" id="icon" width="75" />
-            <div>CSS</div>
+            <div className="skill-name">CSS</div>
           </div>
    
           <div className="inline">
             <Icon icon="simple-icons:python" id="icon" width="75" />
-            <div>Python</div>
+            <div className="skill-name">Python</div>
           </div>
           <div className="inline">
             <Icon icon="akar-icons:django-fill" id="icon" width="75" />
-            <div>Django</div>
+            <div className="skill-name">Django</div>
           </div>
           <div className="inline">
             <Icon icon="teenyicons:mongodb-solid" id="icon" width="75" />
-            <div>MongoDB</div>
+            <div className="skill-name">MongoDB</div>
           </div>
           <div className="inline">
             <Icon icon="akar-icons:postgresql-fill" id="icon" width="75" />
-            <div>PostgreSQL</div>
+            <div className="skill-name">PostgreSQL</div>
           </div>
           <div className="inline">
             <Icon icon="mdi:sql-right-outer-join" id="icon" width="75" />
-            <div>SQL</div>
+            <div className="skill-name">SQL</div>
           </div>
           <div className="inline">
             <Icon icon="fa6-brands:bootstrap" id="icon" width="75" />
-            <div id="bootstrap">Bootstrap</div>
+            <div className="skill-name" id="bootstrap">Bootstrap</div>
           </div>
           <div className="inline">
             <Icon icon="akar-icons:vscode-fill" id="icon" width="75" />
-            <div>VSCode</div>
+            <div className="skill-name">VSCode</div>
           </div>
           <div className="inline">
             <Icon icon="mdi:git" id="icon" width="75" />
-            <div>Git</div>
+            <div className="skill-name">Git</div>
           </div>
           <div className="inline">
             <Icon icon="mdi:github" id="icon" width="75" />
-            <div>GitHub</div>
+            <div className="skill-name">GitHub</div>
           </div>
         </div>
       </div>
