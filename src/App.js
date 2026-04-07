019df533645db3c9cb0a08166cfca88341fa132e
Sasha Ward <sashaward@MacBookPro.mynet>
2026-04-07 23:14:20 +0100

    Retiring site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { Root } from "./components/Root";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
 export const App = () => {
-  const theme = extendTheme({
-    config: {
-      cssVarPrefix: "ck",
-    },
-    colors: {
-      transparent: "transparent",
-      black: "#000",
-      white: "#fff",
-      gray: {
-        50: "#f7fafc",
-        900: "#171923",
-      },
-    },
-  });
+
   return (
-    <ChakraProvider theme={theme}>
-      <Root />
-    </ChakraProvider>
+    window.location.assign('https://www.google.com')
   );
 };
 
